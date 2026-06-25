95f314a39093ca0ec41a7ef346f092dcdb783d3e
iciclemclem <clement.laversin@hotmail.fr>
2026-06-25 22:57:26 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,39 @@
+const lines = {
+
+    1:{
+        color:"#FFCD00",
+        stations:[
+            "La Défense",
+            "Charles de Gaulle Étoile",
+            "Concorde",
+            "Châtelet",
+            "Bastille",
+            "Nation"
+        ]
+    },
+
+    4:{
+        color:"#BE418D",
+        stations:[
+            "Porte de Clignancourt",
+            "Barbès Rochechouart",
+            "Gare du Nord",
+            "Châtelet",
+            "Saint-Michel",
+            "Montrouge"
+        ]
+    },
+
+    14:{
+        color:"#62259D",
+        stations:[
+            "Saint-Denis Pleyel",
+            "Gare Saint-Lazare",
+            "Pyramides",
+            "Châtelet",
+            "Bercy",
+            "Aéroport d'Orly"
+        ]
+    }
+
+};
